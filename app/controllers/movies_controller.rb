41b052338be94d0fb369ca9cfa482abbd56a2c39
ulysse galet <ulysse.galet@rennes-sb.com>
2022-07-26 23:35:40 +0200

    movie, show, index, create, update, delete ok

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,5 +8,50 @@ class MoviesController < ApplicationController
         @movie = Movie.find(params[:id])
     end
 
-    
+    def new
+        @movie = Movie.new
+    end
+
+    def create
+        @movie = Movie.new(movie_params)
+
+        if @movie.save
+            redirect_to @movie
+        else
+         render :new, status: :unprocessable_entity
+        end
+    end
+
+    def edit
+        @movie = Movie.find(params[:id])
+    end
+
+    def update
+        @movie = Movie.find(params[:id])
+
+        if @movie.update(movie_params_update)
+            redirect_to @movie
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @movie = Movie.find(params[:id])
+        @movie.destroy
+        
+
+        redirect_to movies_path
+    end
+
+    private
+
+    def movie_params
+        params.require(:movie).permit(:title, :overview)
+    end
+
+    def movie_params_update
+        params.require(:movie).permit(:title, :overview, :rating, :poster_url)
+    end
+
 end

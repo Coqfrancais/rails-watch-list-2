65f2231607b1d9799c38ce156df0667cf3e6d040
ulysse galet <ulysse.galet@rennes-sb.com>
2022-07-27 00:36:56 +0200

    list added

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,53 @@
 class ListsController < ApplicationController
 
-    
+    def index 
+        @lists = List.all
+    end
+
+    def show
+        @list = List.find(params[:id])
+    end
+
+    def new
+        @list = List.new
+    end 
+
+    def create
+        @list = List.create(params_list)
+
+        if @list.save
+            redirect_to @list
+        else 
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    def edit
+        @list = List.find(params[:id])
+    end
+
+    def update
+        @list = List.find(params[:id])
+        
+        if List.update(params_list)
+            redirect_to @list
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @list = List.find(params[:id])
+        @list.destroy
+
+        redirect_to lists_path
+
+    end 
+
+
+    private
+
+    def params_list
+        params.require(:list).permit(:name)
+    end
 end
